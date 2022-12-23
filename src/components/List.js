26e57de0481885bc7238efb02043c681cb6ee003
Vitalii <vitaliiasmolov@gmail.com>
2022-12-23 22:24:40 +0200

    install fortawesome

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { fontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 export default class List extends Component {
   render() {
